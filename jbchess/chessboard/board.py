a58dc2de7bb62fee0d57d88c0b73f62a798248e4
benedikt-voelkel <benedikt.voelkel@gmx.de>
2020-01-31 00:28:23 +0100

    Board example (#5)
    * Add board collection
    
    A board collection summarises boards of one piece type. It checks for
    overlapping pieces as well.
    
    Add some content and logic
    
    * Add BoardCollection which combines BoardBase objects. Check for
      overlapping fields
    
    * Add (very) primitive drawing of chessboard

--- jbchess/chessboard/board.py
+++ jbchess/chessboard/board.py
@@ -1,5 +1,7 @@
 # This is for now just an example implementation
 
+import sys
+
 
 class BoardBase:
     """Base class for anything which can be placed on the board
@@ -8,7 +10,7 @@ class BoardBase:
     ==> Operators to be overridden
 
     """
-    def __init__(self, occupied=0):
+    def __init__(self, occupied=0, **kwargs):
         """Init chess boar
 
         Args:
@@ -16,12 +18,39 @@ class BoardBase:
         """
         # 64-bit number with bits set to 1 for each field occupied
         self.occupied = occupied
+        self.symbols = None
+
+        self.symbol = kwargs.get("symbol", "x")
+
+
+    def prepare_symbol_list(self):
+        if not self.symbols:
+            self.symbols = [None] * 64
+        for i in range(64):
+            if self.occupied & (1 << i):
+                self.symbols[i] = self.symbol
+                continue
+            self.symbols[i] = None
+
+
+    def __str__(self):
+        self.prepare_symbol_list()
+        string = ""
+        for i, s in enumerate(self.symbols):
+            if i % 8 == 0:
+                string = string + "\n---------------------------------\n|"
+            if s:
+                string = string + f" {s} |"
+                continue
+            string = string + "   |"
+        string = string + "\n---------------------------------\n"
+        return string
 
 
 class PiecesBase(BoardBase):
     """Base class for all types of chess pieces
 
-    This defines e.g. how a chess piece type can be moved 
+    This defines e.g. how a chess piece type can be moved
     """
 
     def __init__(self, name, occupied=0):
@@ -40,4 +69,66 @@ class PiecesBase(BoardBase):
         """
 
 
+class BoardCollection(BoardBase):
+    """A collection of boards
 
+    Can be used to merge boards of different pieces
+
+    """
+
+    def __init__(self, *boards):
+        """Initialise with boards
+
+        Collect all boards
+
+        """
+        super().__init__()
+        self.boards = boards
+
+        self.update_board()
+
+
+    def check_sanity(self):
+        """Quick sanity check for overlapping pieces
+        """
+        occupied_or = 0
+        occupied_xor = 0
+
+        # Same fields have to (not) be occupied by OR and XOR operation
+        for b in self.boards:
+            occupied_or = occupied_or | b.occupied
+            occupied_xor = occupied_xor ^ b.occupied
+            if occupied_or != occupied_xor:
+                return False
+        return True
+
+
+    def update_board(self):
+        """Update board
+        """
+        if not self.check_sanity():
+            print("ERROR: Overlapping pieces")
+            sys.exit(1)
+        self.occupied = 0
+        for b in self.boards:
+            self.occupied = self.occupied | b.occupied
+
+
+    def __str__(self):
+        symbols = [None] * 64
+        for b in self.boards:
+            b.prepare_symbol_list()
+            for i, s in enumerate(b.symbols):
+                if s:
+                    symbols[i] = s
+
+        string = ""
+        for i, s in enumerate(symbols):
+            if i % 8 == 0:
+                string = string + "\n---------------------------------\n|"
+            if s:
+                string = string + f" {s} |"
+                continue
+            string = string + "   |"
+        string = string + "\n---------------------------------\n"
+        return string
--- jbchess/play.py
+++ jbchess/play.py
@@ -1,4 +1,4 @@
-
+from jbchess.chessboard.board import BoardBase, BoardCollection
 
 
 def play():
@@ -7,3 +7,17 @@ def play():
     This is the steering function to be called when a new game should be played
     """
     print("Play JBChess")
+
+    board = BoardBase(1, symbol="o")
+
+    board2 = BoardBase(2, symbol="H")
+    board3 = BoardBase(4, symbol="O")
+
+    print(board)
+
+    print(board2)
+    print(board3)
+
+    board_coll = BoardCollection(board, board2, board3)
+
+    print(board_coll)
